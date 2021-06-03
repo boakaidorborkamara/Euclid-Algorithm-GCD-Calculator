f3dabaeb7525642b77f27c5a0076f7fdf2865a09
Boakai Dorbor Kamara <boakaidkamara@gmail.com>
2021-06-03 23:51:57 +0000

    Commit after getting functionality right

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
+//submit btn
+let calculate_btn = document.getElementById("calculate-btn");
 
-//btn
-    let calculate_btn = document.getElementById("calculate-btn");
-    
-    let result_area = document.getElementById("result-area");
-    console.log(result_area);
+//show result area
+let result_area = document.getElementById("result-area");
+// console.log(result_area);
 
-    //calculate gcd
-    function gcdCalculator(){
-       let reminder ;
-       let greatest_common_factor;
-       let num1 = parseInt( document.getElementById("num1").value);
-       let num2 = parseInt( document.getElementById("num2").value);
+
+//calculate and find the  greatest common divisor
+function gcdCalculator(){
+   
+   //user values
+   let first_number = parseInt( document.getElementById("num1").value);
+   let second_number = parseInt( document.getElementById("num2").value);
+
+   let reminder;
+
+   if(first_number > second_number){
+        //first divison
+        reminder = first_number % second_number;
+
+        if(reminder === 0){
+            console.log(second_number + " is the GCD");
+        }
+        else{
+            while( reminder !== 0){
+                console.log(reminder);
+                first_number = reminder;
+                reminder = second_number % first_number;
+                
+                console.log(first_number + " first number");
+                console.log(reminder);
+                // console.log(second_number + " second num");
+            }
+            
+            if(reminder == 0){
+                console.log(first_number + " is the GCD");
+            }
+            
+        }
         
-       if(num1 > num2){
-            reminder = num1 % num2;
+   }
+  
+   
+   
 
-            if(reminder === 0){
-                greatest_common_factor = num2;
-                result_area.innerHTML = `The greatest common divisor is ${greatest_common_factor}`;
-            }
-            else if(reminder != 0){
-                reminder = num2 % reminder;
-                result_area.innerHTML = `The greatest common divisor is ${num2}`;
-            }
-        
-       }
-       else if(num2 > num1){
-           greatest_common_factor = num1;
-           reminder = num2 % num1;
-           result_area.innerHTML = `The greatest common divisor is ${greatest_common_factor}`;
+}
 
-           if(reminder === 0){
-                greatest_common_factor = num1;
-                result_area.innerHTML = `The greatest common divisor is ${greatest_common_factor}`;
-            }
-            else if(reminder != 0){
-                reminder = num2 % reminder;
-                result_area.innerHTML = `The greatest common divisor is ${num1}`;
-            }
-       }
-       
-    }
-
-    calculate_btn.addEventListener("click",gcdCalculator );
+calculate_btn.addEventListener("click",gcdCalculator );
